fba78a2a14b583bbe8ba42aeff4496f0bd6f5e8a
liwenhao <wenhao.li08@ele.e>
2018-05-08 10:57:28 +0800

    fork-workflow

--- fork-workflow-update.sh
+++ fork-workflow-update.sh
@@ -1,17 +1,19 @@
-git remote add upstream git@github.com:geeeeeeeeek/git-recipes.git
-此处可使用 git remote -v 查看远程分支列表
+# 进入项目
+cd /Users/liwenhao/Desktop/IDEAProject/cps
+# 添加远程分支
+git remote add upstream git@git.elenet.me:testinfra/cps.git
+# 此处可使用 git remote -v 查看远程分支列表
+git remote -v
+# fetch 源分支的新版本到本地
+git fetch upstream
+# 切换到本地master分支
+git checkout master
+# 合并两个版本的代码
+git merge upstream/master
+# 切换到本地develop分支
+git checkout develop
+# 合并两个版本的代码
+git merge upstream/develop
+# 删除远程分支
+git remote remove upstream
 
-$ git remote -v
-origin	git@github.com:morethink/git-recipes.git (fetch)
-origin	git@github.com:morethink/git-recipes.git (push)
-upstream	git@github.com:geeeeeeeeek/git-recipes.git (fetch)
-upstream	git@github.com:geeeeeeeeek/git-recipes.git (push)
-fetch 源分支的新版本到本地
-$ git fetch upstream
-
-切换到本地master分支
-$ git checkout master
-合并两个版本的代码
-$ git merge upstream/master
-将合并后的代码 push 到 GitHub 上去
-$ git push origin master
